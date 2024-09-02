0b8b427fc1d9e05999ae000dc514ff2e42d83196
manam <manam@MAHESHREDDY>
2024-09-02 13:12:58 +0530

    A

--- KeyVault/Controllers/WeatherForecastController.cs
+++ KeyVault/Controllers/WeatherForecastController.cs
@@ -8,7 +8,7 @@ namespace KeyVault.Controllers
     {
         private static readonly string[] Summaries = new[]
         {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+            "A", "B", "C", "D", "V", "A", "A", "S", "W", "F"
         };
 
         private readonly ILogger<WeatherForecastController> _logger;
